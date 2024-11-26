c09a6aff30fe0a8053cd7106ee80b13a555aee70
kilimichenkoDaniel <kilimichenko_D@itstep.org>
2024-11-26 18:35:03 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Changers from Github");
+Console.WriteLine("Changers from Github 2 213423423 ");
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Changers from Github");
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Changers from Github");
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Changers from Github");
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Changers from Github");
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Changers from Github");
 
